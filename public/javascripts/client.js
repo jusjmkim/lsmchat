75486353659533f104d6873c4849ed3eda6aef99
Justin Kim <justinjmkim@gmail.com>
2014-08-19 17:47:16 -0400

    Adjust heading

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -72,7 +72,7 @@ function displayOwnName(name) {
 }
 
 function welcomeUser(name) {
-  return "<h2>Welcome <span id='username'>" + name + "</span>!</h2>";
+  return "<h2>Hey <span id='username'>" + name + "</span>!</h2>";
 }
 
 function scrapeUsername() {
